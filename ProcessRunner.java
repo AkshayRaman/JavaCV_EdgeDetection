2ef6d0e48e55c3280089dff5bc6e66023d41a4b2
Akshay Raman <AkshayRaman@users.noreply.github.com>
2016-07-10 10:49:50 +0000

    Update ProcessRunner.java
    Cosmetic changes

--- ProcessRunner.java
+++ ProcessRunner.java
@@ -1,18 +1,16 @@
 import java.io.IOException;
 
-
 public class ProcessRunner {
-	
-	public static void runProcess(String Executable)
-	{
-		if(Executable.trim().length()>0){
+
+	public static void runProcess(String Executable) {
+		if (Executable.trim().length() > 0) {
 			try {
-				Runtime.getRuntime().exec("cmd /c start "+"\""+Executable+"\"");
+				Runtime.getRuntime().exec("cmd /c start " + "\"" + Executable + "\"");
 			} catch (IOException e) {
-				System.out.println("ERROR: ProcessRunner "+e.getMessage());
+				System.out.println("ERROR: ProcessRunner " + e.getMessage());
 				e.printStackTrace();
 			}
-	}
+		}
 
-}
+	}
 }
